532cd0ae3e067a2ff75b7c94c5cfd19f4b43ba8b
Intisar Sikder <sikder.intisar@gmail.com>
2023-03-23 17:58:02 +0000

    Produced the guessing number game

---
+++ src/main/java/org/example/GuessingGame.java
@@ -0,0 +1,35 @@
+package org.example;
+import java.util.Scanner;
+
+public class GuessingGame {
+    public static void main(String[] args) {
+
+//        Prompt the user to input a number of their choice
+        System.out.println("Using digits, can you guess what number I am thinking of? ");
+
+//               Collect user input
+        Scanner reader = new Scanner(System.in); // this creates a scanner object
+        Integer secretnumber = Integer.valueOf(reader.nextLine()); // this reads the user input
+
+//        if number = 7, print "Well done!"
+//        otherwise print "Unfortunately, that's not the number I was thinking of :("
+
+        if (secretnumber == 7){
+
+            System.out.println("Well done!");
+
+        } else if (secretnumber < 7){
+
+            System.out.println("That's too low, try again!");
+
+        }
+
+        else {
+
+            System.out.println("That's too high, try again!");
+
+        }
+
+        }
+    }
+
